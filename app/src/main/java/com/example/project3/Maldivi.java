ba98cef37b0134d2d2b27e5533162247acf1d970
Peter <yuukit565@gmail.com>
2020-10-27 14:18:25 +0900

    Initial commit

---
+++ app/src/main/java/com/example/project3/Maldivi.java
@@ -0,0 +1,13 @@
+package com.example.project3;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Maldivi extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.maldivi);
+    }
+}
